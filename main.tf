d6bc559c11cac9b80c80d16fbc1bf82901c4a09f
Tio Bagio <10209203+tiobagio@users.noreply.github.com>
2024-01-25 12:34:05 +0800

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,53 @@
+locals {
+  unused_limit = timeadd(timestamp(), "-720h")
+}
+
+data "aws_iam_access_keys" "example" {
+  user = "raymond"
+}
+
+output "iam_access_key_output" {
+  value = data.aws_iam_access_keys.example
+}
+
+locals {
+  key_list = tolist(data.aws_iam_access_keys.example.access_keys)
+}
+
+locals {
+  iam_key_create_date = local.key_list[1].create_date
+}
+
+output "iam_access_key_create_date_output" {
+  value = local.iam_key_create_date
+}
+
+output "iam_access_key_user_output" {
+  value = data.aws_iam_access_keys.example.user
+}
+
+locals {
+  c_data = coalesce(local.iam_key_create_date, local.unused_limit)
+}
+
+output "unused_limit_output" {
+  value = local.unused_limit
+}
+
+
+output "c_data_output" {
+  value = local.c_data
+}
+
+
+check "check_iam_role_unused" {
+  assert {
+    condition = (
+      timecmp(
+        coalesce(local.iam_key_create_date, local.unused_limit),
+        local.unused_limit,
+      ) > 0
+    )
+    error_message = "error_message here"
+  }
+}
